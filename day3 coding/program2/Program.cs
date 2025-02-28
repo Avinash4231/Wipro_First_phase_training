08b60881ca980f21bdd21e9ab4b94ab1299d5f25
Avinash Kumar <119235391+Avinash4231@users.noreply.github.com>
2025-03-01 00:19:00 +0530

    Add files via upload

---
+++ day3 coding/program2/Program.cs
@@ -0,0 +1,39 @@
+﻿int n1 = 1, n2 = 3, n3 = 3;
+
+switch (n1 + n2 + n3)
+
+{
+
+case 0:
+
+case 2:
+
+case 4:
+
+++n1;
+
+n3 += n2;
+
+     break;
+
+case 1:
+
+case 3:
+
+case 5:
+
+--n1;
+
+n3 -= n2;
+
+break;
+
+default:
+
+      n1 += n2;
+
+break;
+
+}
+
+Console.WriteLine(n1 + "\n" + n2 + "\n" + n3);
