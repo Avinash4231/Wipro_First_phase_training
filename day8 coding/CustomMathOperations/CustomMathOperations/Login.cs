ea04428017b5504ac63b1ec48a9a34ca028502c0
Avinash Kumar <119235391+Avinash4231@users.noreply.github.com>
2025-03-01 00:37:00 +0530

    Add files via upload

---
+++ day8 coding/CustomMathOperations/CustomMathOperations/Login.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomMathOperations
+{
+    public class Login
+    {
+        public string DoLogin(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return "Username and password cannot be empty";
+            }
+
+            if (username == "admin" && password == "admin")
+            {
+                return "Login successful";
+            }
+
+            return "Invalid credentials";
+        }
+    }
+
+}
---
+++ day8 coding/CustomMathOperations/MSTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MSTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MSTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c15b559-cdcb-4daf-bea5-9033fadb0eff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
