b8da84f0f540bcd2d34137d6da5ee303b71060a7
Avinash Kumar <119235391+Avinash4231@users.noreply.github.com>
2025-03-01 00:41:03 +0530

    Add files via upload

---
+++ day8 coding/CustomMathOperations/NUnitTesting/LoginTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomMathOperations;
+
+namespace NUnitTesting
+{
+    [TestFixture]
+    public class LoginTests
+    {
+        private Login login;
+
+        [SetUp]
+        public void Setup()
+        {
+            login = new Login();
+        }
+
+        [Test]
+        public void Check_Username_Null()
+        {
+            string result = login.DoLogin(null, "admin");
+            Assert.That(result, Is.EqualTo("Username and password cannot be empty"));
+        }
+
+        [Test]
+        public void Check_Password_Null()
+        {
+            string result = login.DoLogin("admin", null);
+            Assert.That(result, Is.EqualTo("Username and password cannot be empty"));
+        }
+
+        [Test]
+        public void Check_Invalid_Credentials()
+        {
+            string result = login.DoLogin("user", "password");
+            Assert.That(result, Is.EqualTo("Invalid credentials"));
+        }
+
+        [Test]
+        public void Check_Login_Successful()
+        {
+            string result = login.DoLogin("admin", "admin");
+            Assert.That(result, Is.EqualTo("Login successful"));
+        }
+    }
+}
---
+++ day8 coding/CustomMathOperations/NUnitTesting/MathNUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomMathOperations;
+
+namespace NUnitTesting
+{
+    [TestFixture]
+    public class MathNUnitTest
+    {
+        BasicMath bm=new BasicMath();
+        [Test]
+        public void Test_Add()
+        {
+            int result = bm.Add(4, 5);
+
+            int expected = 9;
+            Assert.That(result.Equals(expected));
+        }
+
+        [Test]
+        public void Test_Subtract()
+        {
+            int result = bm.Subtract(4, 5);
+
+            int expected = -1;
+            Assert.That(result.Equals(expected));
+        }
+
+        [Test]
+        public void Test_Multiply()
+        {
+            int result = bm.Mutiply(4, 5);
+
+            int expected = 20;
+            Assert.That(result.Equals(expected));
+        }
+
+        [Test]
+        public void Test_Division()
+        {
+            int result = bm.Divide(4, 5);
+
+            int expected = 0;
+            Assert.That(result.Equals(expected));
+        }
+    }
+}
